0a5d8a02bd225b667e2409043ff5110e0ceb1fe2
venkontol <xcblate@gmail.com>
2026-09-05 15:36:22 +0700

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import router from "./router.js";
-
-export default {
-  async fetch(request, env, ctx) {
-    return await router(request, env, ctx);
-  }
-};
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export function getSecurityHeaders() {
-  return {
-    "X-Content-Type-Options": "nosniff",
-    "X-Frame-Options": "DENY",
-    "Referrer-Policy": "strict-origin-when-cross-origin",
-    "Permissions-Policy": "camera=(), microphone=(), geolocation=()",
-    "Content-Security-Policy": "default-src 'self'; img-src 'self' data: https:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline';"
-  };
-}
